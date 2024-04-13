b51e27145e88236f9402247adee1db2f67491eac
AsPingvinas <cicdomantasp@gmail.com>
2024-04-13 12:49:43 +0300

    Seperated classes related to character races to seperate file

--- character_creator.py
+++ character_creator.py
@@ -1,7 +1,8 @@
 import random
 import os
 from enum import Enum
-from race import Race
+from race import Race, RaceAttribute
+
 
 class CharacterClass(Enum):
     CLERIC = "Cleric"
@@ -43,118 +44,6 @@ class Character:
         constitution = self.ability_scores['Constitution']
         hp = random.randint(1, hit_die)
         return hp + constitution
-
-class RaceAttribute(Character):
-    @staticmethod
-    def Human(ability_scores):
-        speed = "Base walking speed is 30 feet"
-        size = "Medium"
-        for ability in ability_scores:
-            ability_scores[ability] += 1
-        traits = {
-            "Extra language\n"
-        }
-        return ability_scores, size, speed, traits
-
-    @staticmethod
-    def Elf(ability_scores):
-        speed = "Base walking speed is 30 feet"
-        size = "Medium"
-        ability_scores['Dexterity'] += 2
-        traits = {
-            "Darkvision\n"
-            "Keen Senses\n"
-            "Fey Ancestry\n"
-            "Trance\n"
-        }
-        return ability_scores, size, speed, traits
-    @staticmethod
-    def Dwarf(ability_scores):
-        speed = "Base walking speed is 25 feet"
-        size = "Medium"
-        ability_scores['Constitution'] += 2
-        traits = {
-            "Darkvision\n"
-            "Dwarven resilience\n"
-            "Dwarven combat training\n"
-            "Stonecunning\n"
-        }
-        return ability_scores, size, speed, traits
-
-    @staticmethod
-    def Halfling(ability_scores):
-        speed = "Base walking speed is 25 feet"
-        size = "Small"
-        ability_scores['Dexterity'] += 2
-        traits = {
-            "Lucky\n"
-            "Brave\n"
-            "Halfling nimbleness\n"
-        }
-        return ability_scores, size, speed, traits
-
-    @staticmethod
-    def Dragonborn(ability_scores):
-        speed = "Base walking speed is 30 feet"
-        size = "Medium"
-        ability_scores['Strength'] += 2
-        ability_scores['Charisma'] += 1
-        traits = {
-            "Draconic ancestry\n"
-            "Breath weapon\n"
-            "Damage resistance\n"
-        }
-        return ability_scores, size, speed, traits
-
-    @staticmethod
-    def Gnome(ability_scores):
-        speed = "Base walking speed is 25 feet"
-        size = "Small"
-        ability_scores['Intelligence'] += 2
-        traits = {
-            "Darkvision\n"
-            "Gnome Cunning\n"
-        }
-        return ability_scores, size, speed, traits
-
-    @staticmethod
-    def Half_elf(ability_scores):
-        speed = "Base walking speed is 30 feet"
-        size = "Medium"
-        ability_scores['Charisma'] += 2
-        traits = {
-            "Darkvision\n"
-            "Fey ancestry\n"
-            "Skill versatility\n"
-        }
-        return ability_scores, size, speed, traits
-
-    @staticmethod
-    def Half_orc(ability_scores):
-        speed = "Base walking speed is 30 feet"
-        size = "Medium"
-        ability_scores['Strength'] += 2
-        ability_scores['Constitution'] += 1
-        traits = {
-            "Darkvision\n"
-            "Menacing\n"
-            "Relentless endurance\n"
-            "Savage attacks\n"
-        }
-        return ability_scores, size, speed, traits
-
-    @staticmethod
-    def Tiefling(ability_scores):
-        speed = "Base walking speed is 30 feet"
-        size = "Medium"
-        ability_scores['Charisma'] += 2
-        ability_scores['Intelligence'] += 1
-        traits = {
-            "Darkvision\n"
-            "Hellish resistance\n"
-            "Infernal legacy\n"
-        }
-        return ability_scores, size, speed, traits
 
 def create_character_file():
     character_name = input("Enter your character's name: ")
--- race.py
+++ race.py
@@ -10,3 +10,114 @@ class Race(Enum):
     HUMAN = "Human"
     TIEFLING = "Tiefling"
 
+class RaceAttribute(Character):
+    @staticmethod
+    def Human(ability_scores):
+        speed = "Base walking speed is 30 feet"
+        size = "Medium"
+        for ability in ability_scores:
+            ability_scores[ability] += 1
+        traits = {
+            "Extra language\n"
+        }
+        return ability_scores, size, speed, traits
+
+    @staticmethod
+    def Elf(ability_scores):
+        speed = "Base walking speed is 30 feet"
+        size = "Medium"
+        ability_scores['Dexterity'] += 2
+        traits = {
+            "Darkvision\n"
+            "Keen Senses\n"
+            "Fey Ancestry\n"
+            "Trance\n"
+        }
+        return ability_scores, size, speed, traits
+    @staticmethod
+    def Dwarf(ability_scores):
+        speed = "Base walking speed is 25 feet"
+        size = "Medium"
+        ability_scores['Constitution'] += 2
+        traits = {
+            "Darkvision\n"
+            "Dwarven resilience\n"
+            "Dwarven combat training\n"
+            "Stonecunning\n"
+        }
+        return ability_scores, size, speed, traits
+
+    @staticmethod
+    def Halfling(ability_scores):
+        speed = "Base walking speed is 25 feet"
+        size = "Small"
+        ability_scores['Dexterity'] += 2
+        traits = {
+            "Lucky\n"
+            "Brave\n"
+            "Halfling nimbleness\n"
+        }
+        return ability_scores, size, speed, traits
+
+    @staticmethod
+    def Dragonborn(ability_scores):
+        speed = "Base walking speed is 30 feet"
+        size = "Medium"
+        ability_scores['Strength'] += 2
+        ability_scores['Charisma'] += 1
+        traits = {
+            "Draconic ancestry\n"
+            "Breath weapon\n"
+            "Damage resistance\n"
+        }
+        return ability_scores, size, speed, traits
+
+    @staticmethod
+    def Gnome(ability_scores):
+        speed = "Base walking speed is 25 feet"
+        size = "Small"
+        ability_scores['Intelligence'] += 2
+        traits = {
+            "Darkvision\n"
+            "Gnome Cunning\n"
+        }
+        return ability_scores, size, speed, traits
+
+    @staticmethod
+    def Half_elf(ability_scores):
+        speed = "Base walking speed is 30 feet"
+        size = "Medium"
+        ability_scores['Charisma'] += 2
+        traits = {
+            "Darkvision\n"
+            "Fey ancestry\n"
+            "Skill versatility\n"
+        }
+        return ability_scores, size, speed, traits
+
+    @staticmethod
+    def Half_orc(ability_scores):
+        speed = "Base walking speed is 30 feet"
+        size = "Medium"
+        ability_scores['Strength'] += 2
+        ability_scores['Constitution'] += 1
+        traits = {
+            "Darkvision\n"
+            "Menacing\n"
+            "Relentless endurance\n"
+            "Savage attacks\n"
+        }
+        return ability_scores, size, speed, traits
+
+    @staticmethod
+    def Tiefling(ability_scores):
+        speed = "Base walking speed is 30 feet"
+        size = "Medium"
+        ability_scores['Charisma'] += 2
+        ability_scores['Intelligence'] += 1
+        traits = {
+            "Darkvision\n"
+            "Hellish resistance\n"
+            "Infernal legacy\n"
+        }
+        return ability_scores, size, speed, traits
